8b53250cb09dcf40a9a00c4827796939681fca73
koradeej <67166601+koradeej@users.noreply.github.com>
2020-06-23 19:20:29 -0400

    Update PalindromeCheck.java

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -1,16 +1,23 @@
 
+/*
+This project is checking if a given set of four digits is a palindrome. It initially prompts the user if
+they want to enter a number of have it randomly generated, then it analyzes it.
+*/
+
+
+
 import java.util.Scanner;
 public class PalindromeCheck{
 	public static void main(String[] args){
 		
-		System.out.print("Enter 0 if you want to input your own 4-digit number. Enter 1 if you want it randomly generated: ");	//prompts about if they want it randomized or custom
+		System.out.println("Enter 0 if you want to enter a 4-digit number. Enter 1 if you would like to have the computer generate it. ");	//prompts about if they want it randomized or custom
 		Scanner input = new Scanner(System.in);
 		int answer = input.nextInt();
 		String finalStr = "0000";
 		int finalNum = 0;
 		
 		if(answer==0){    								//prompts user for 4 digit code
-			System.out.print("Enter a 4 digit integer: ");
+			System.out.println("Enter a 4 digit integer: ");
 			int choseNum = input.nextInt();	
 			finalStr = Integer.toString(choseNum);		//makes a string of the input
 			finalNum = choseNum;						//copies the number of the input for math (checking if its right size)
@@ -29,21 +36,21 @@ public class PalindromeCheck{
 		
 		
 		if(finalNum <1000){  							//Makes sure number is the right size 
-			System.out.println("That number is too small! make it four digits.");
+			System.out.println("Incorrect input");
 		}
 		else if (finalNum > 9999){						//
-			System.out.println("That number is too big! make it four digits.");
+			System.out.println("Incorrect input");
 		}
 		
 		else {
 			
 			if(finalStr.charAt(0) == finalStr.charAt(3)){				//Check if first and last char in string are the same
 				if(finalStr.charAt(1) == finalStr.charAt(2)){			//Check if second and third characters match
-					System.out.println("That's a palindrome!");
+					System.out.println(finalNum + " is a palindrome!");
 				}
 			}
 			else{
-				System.out.println("Thats not a palindrome.");
+				System.out.println(finalNum + " is not a palindrome.");
 			}	
 		}
 	}
